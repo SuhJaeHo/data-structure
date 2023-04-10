b0e0d3bb96255ba725198dcf81df81041ae89de3
jaeho seo <>
2023-04-10 21:34:12 +0900

    create deque

---
+++ test_deque.mjs
@@ -0,0 +1,51 @@
+import { Deque } from "./Deque.mjs";
+
+const deque = new Deque();
+
+console.log("addFirst method called");
+deque.addFirst(1);
+deque.addFirst(2);
+deque.addFirst(3);
+deque.addFirst(4);
+deque.addFirst(5);
+deque.printAll();
+console.log(`isEmpty: ${deque.isEmpty()}`);
+console.log("\n");
+
+console.log("removeFirst method called");
+deque.removeFirst();
+deque.printAll();
+deque.removeFirst();
+deque.printAll();
+deque.removeFirst();
+deque.printAll();
+deque.removeFirst();
+deque.printAll();
+deque.removeFirst();
+deque.printAll();
+console.log(`isEmpty: ${deque.isEmpty()}`);
+console.log("\n");
+
+console.log("addLast method called");
+deque.addLast(1);
+deque.addLast(2);
+deque.addLast(3);
+deque.addLast(4);
+deque.addLast(5);
+deque.printAll();
+console.log(`isEmpty: ${deque.isEmpty()}`);
+console.log("\n");
+
+console.log("removeLast method called");
+deque.removeLast();
+deque.printAll();
+deque.removeLast();
+deque.printAll();
+deque.removeLast();
+deque.printAll();
+deque.removeLast();
+deque.printAll();
+deque.removeLast();
+deque.printAll();
+console.log(`isEmpty: ${deque.isEmpty()}`);
+console.log("\n");
